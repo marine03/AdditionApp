bfdf2b7f3001f57e389f53dcd41cbb1c3c07fd93
marine03 <stevenwusmc@gmail.com>
2019-09-14 11:30:12 -0400

    simple addition on android

---
+++ app/src/main/java/com/example/app1addition/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.app1addition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button addBtn = (Button) findViewById(R.id.additionBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText first = (EditText) findViewById(R.id.firstEditText);
+                EditText second = (EditText) findViewById(R.id.secondEditText2);
+                TextView ans = (TextView) findViewById(R.id.ansTextView);
+
+                int num1= Integer.parseInt(first.getText().toString());
+                int num2= Integer.parseInt(second.getText().toString());
+                int result = num1+num2;
+                ans.setText(result + "");
+
+            }
+        });
+
+
+
+    }
+}
